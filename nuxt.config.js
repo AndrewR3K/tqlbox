7a196c538b92786a079227df2436395a56ce3924
AndrewR3K <anmartin@csumb.edu>
2020-07-24 18:47:05 -0700

    adding static generation and dist for netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'server',
-  generate: {
-    fallback: true
-  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
